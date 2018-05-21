d84543284df981b5fa5d6996c350d5240020c064
amSalehoof <a.m.salehoof@gmail.com>
2018-05-21 17:13:33 +0430

    Add Arbitrary Locations Page
    Signed-off-by: amSalehoof <a.m.salehoof@gmail.com>

--- routes/pages.go
+++ routes/pages.go
@@ -242,6 +242,19 @@ func LocationsOnMap(c *gin.Context) {
 	})
 }
 
+func ArbitraryLocations(c *gin.Context) {
+	groupName := c.Param("group")
+	if _, err := os.Stat(path.Join(glb.RuntimeArgs.SourcePath, groupName+".db")); os.IsNotExist(err) {
+		c.HTML(http.StatusOK, "changedb.tmpl", gin.H{
+			"ErrorMessage": "First download the app or CLI program to insert some fingerprints.",
+		})
+		return
+	}
+	c.HTML(http.StatusOK, "arbitrary_locations.tmpl", gin.H{
+		"Group": groupName,
+	})
+}
+
 // slash Location returns location (to be deprecated)
 //func SlashLocation(c *gin.Context) {
 //	groupName := c.Param("group")
