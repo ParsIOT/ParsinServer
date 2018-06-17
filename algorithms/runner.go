b2611ebf86c23fb43656a2883f37def86e58ae27
hadiazaddel <hadiazaddel@gitlab.com>
2018-06-17 10:55:34 +0430

    removeoutlines
    Signed-off-by: hadiazaddel <hadiazaddel@gitlab.com>

--- algorithms/runner.go
+++ algorithms/runner.go
@@ -956,9 +956,9 @@ func PreProcess(groupName string) {
 	}
 
 	// regulating rss data
-	//for loc,rdData := range locRDMap{
-	//	locRDMap[loc] = RemoveOutlines(rdData)
-	//}
+	for loc, rdData := range locRDMap {
+		locRDMap[loc] = RemoveOutlines(rdData)
+	}
 
 	// converting locRDMap to rd
 	tempFingerprintsData := make(map[string]parameters.Fingerprint)
