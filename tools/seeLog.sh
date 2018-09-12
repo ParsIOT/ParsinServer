40d7e1c4134e164b2401af3a909141952fc597c1
hadiazad <m.h.azaddel@gmail.com>
2018-09-12 15:54:40 +0100

    seeLog screen flickering problem is solved
    Signed-off-by: hadiazad <m.h.azaddel@gmail.com>

--- tools/seeLog.sh
+++ tools/seeLog.sh
@@ -2,18 +2,26 @@
 
 #!/bin/bash
 
-hostAddr=http://195.201.42.209:18003
+#hostAddr=http://195.201.42.209:18003
+hostAddr=http://127.0.0.1:8003
 
 curl -X POST -F 'username=admin' -F 'password=admin' $hostAddr/login --cookie-jar ./ParsinCookie
 
 file=$(mktemp)
 trap 'rm $file' EXIT
 
+#(while true; do
+#    # shellcheck disable=SC2094
+#    sleep 3
+#    curl -s --cookie ./ParsinCookie --fail -r "$(stat -c %s "$file")"- "$hostAddr/data/out.html" 2> "$file "
+#done) &
+
+
 (while true; do
-    # shellcheck disable=SC2094
-    sleep 1
-    curl -s --cookie ./ParsinCookie --fail -r "$(stat -c %s "$file")"- "$hostAddr/data/out.html" 2>> "$file "
+    sleep 1;
+    wget --load-cookies ./ParsinCookie -c -O $file -o /dev/null $hostAddr/data/out.html;
 done) &
+
 pid=$!
 trap 'kill $pid; rm $file; rm ./ParsinCookie' EXIT
 
